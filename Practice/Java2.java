9da9389b0516c2f9d8e853d30ef8073cd767613e
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-02-29 15:02:24 +0530

    Add files via upload

---
+++ Practice/Java2.java
@@ -0,0 +1,26 @@
+// public class Java1 {
+//     public static void main(String[] args) {
+//         String name = "Saitama";
+//         int age = 25;
+//         hello(name, age);
+//     }
+
+//     static void hello(String Name, int Age) {
+//         System.out.println("Hello" + Name);
+//         System.out.println("Hello" + Age);
+//     }
+// }
+
+class Methods2 {
+    public static void main(String[] args) {
+        int x = 34;
+        int y = 45;
+        System.out.println(add(x, y));
+    }
+
+    static int add(int x, int y) {
+        int z = x + y;
+        return z;
+    }
+
+}

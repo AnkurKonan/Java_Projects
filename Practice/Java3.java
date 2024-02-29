78967263e76ba390c3800f2eb52ca5ca27b8acfd
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-02-29 15:34:28 +0530

    Create Java3.java

---
+++ Practice/Java3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Java3 {
+    public static void main(String[] args) {
+        System.out.printf("Its over %d \n", 9000);
+        boolean myBool = true;
+        char myChar = '$';
+        String myString = "bro";
+        int myInt = 34;
+        float myFloat = 54.34f;
+        double myDouble = 500;
+        System.out.printf("%b\n", myBool);
+        System.out.printf("%d\n", myInt);
+        System.out.printf("%s\n", myString);
+        System.out.printf("%c\n", myChar);
+        System.out.printf("%20s\n", myString);
+        System.out.printf("%.10f\n", myDouble);
+        System.out.printf("%f\n", myFloat);
+    }
+}

f86898571385e432ac325844fc33bf1cbf81d5b4
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-02 22:37:26 +0530

    Create Java1.java

---
+++ Practice/Java1.java
@@ -0,0 +1,12 @@
+public class Java1 {
+    public static void main(String[] args) {
+        String name = "Saitama";
+        int age = 25;
+        hello(name, age);
+    }
+
+    static void hello(String Name, int Age) {
+        System.out.println("Hello" + Name);
+        System.out.println("Hello" + Age);
+    }
+}
